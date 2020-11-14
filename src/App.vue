eee595963bdf183fd103bb66572a7776f8b2fa17
xiaosheng <1348909544@qq.com>
2020-11-14 22:05:36 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="stylus">
+*
+  margin 0
+  padding 0
+  box-sizing border-box
+html, body, #app
+  height 100%
+  width 100%
+</style>
