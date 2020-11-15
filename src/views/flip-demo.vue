266f766f4a58c99e5a84a50410fb1a851889050b
xiaosheng <1348909544@qq.com>
2020-11-15 21:37:39 +0800

    fixed滚动

--- src/views/flip-demo.vue
+++ src/views/flip-demo.vue
@@ -4,7 +4,7 @@
       <swiper-slide class="slide slide-1">slide-1</swiper-slide>
       <swiper-slide class="slide slide-2">
         <van-nav-bar
-        title="第二页calc滚动"
+          title="第二页calc滚动"
           left-text="上一页"
           right-text="下一页"
           left-arrow
@@ -14,14 +14,14 @@
         <div class="scroll-wrapper">
           <cube-scroll>
             <div class="scroll-inner">
-              <div v-for="i in 200" :key="i">{{i}}</div>
+              <div v-for="i in 200" :key="i">{{ i }}</div>
             </div>
           </cube-scroll>
         </div>
       </swiper-slide>
       <swiper-slide class="slide slide-3">
         <van-nav-bar
-        title="第三页flex滚动"
+          title="第三页flex滚动"
           left-text="上一页"
           right-text="下一页"
           left-arrow
@@ -31,12 +31,28 @@
         <div class="scroll-wrapper">
           <cube-scroll>
             <div class="scroll-inner">
-              <div v-for="i in 200" :key="i">{{i}}</div>
+              <div v-for="i in 200" :key="i">{{ i }}</div>
             </div>
           </cube-scroll>
         </div>
       </swiper-slide>
-      <swiper-slide class="slide slide-4">slide-4</swiper-slide>
+      <swiper-slide class="slide slide-4">
+        <van-nav-bar
+          title="第四页fixed滚动"
+          left-text="上一页"
+          right-text="下一页"
+          left-arrow
+          @click-left="onClickLeft"
+          @click-right="onClickRight"
+        />
+        <div class="scroll-wrapper">
+          <cube-scroll>
+            <div class="scroll-inner">
+              <div v-for="i in 200" :key="i">{{ i }}</div>
+            </div>
+          </cube-scroll>
+        </div>
+      </swiper-slide>
       <swiper-slide class="slide slide-5">slide-5</swiper-slide>
     </swiper>
   </div>
@@ -44,7 +60,14 @@
 
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import { Swiper as SwiperClass, EffectFlip, EffectCube, EffectCoverflow, EffectFade, Thumbs } from 'swiper/core'
+import {
+  Swiper as SwiperClass,
+  EffectFlip,
+  EffectCube,
+  EffectCoverflow,
+  EffectFade,
+  Thumbs
+} from 'swiper/core'
 import 'swiper/swiper-bundle.css'
 SwiperClass.use([EffectFlip, EffectCube, EffectCoverflow, EffectFade, Thumbs])
 export default {
@@ -88,41 +111,36 @@ export default {
 }
 </script>
 
-<style lang='stylus' scrope>
-.flip {
-  height: 100%;
-  width: 100%;
-  background-color: antiquewhite;
-  .swiper {
-    height: 100%;
-    .slide {
-      height: 100%;
-      width: 100%;
-      &.slide-1{
+<style lang="stylus" scrope>
+.flip
+  height 100%
+  width 100%
+  background-color antiquewhite
+  .swiper
+    height 100%
+    .slide
+      height 100%
+      width 100%
+      &.slide-1
         background-color #f00
-      }
-       &.slide-2{
+      &.slide-2
         background-color #ff0
-        .scroll-wrapper{
+        .scroll-wrapper
           height calc(100% - 46px)
-        }
-      }
-       &.slide-3{
+      &.slide-3
         background-color #fff
         display flex
         flex-direction column
-        .scroll-wrapper{
+        .scroll-wrapper
           flex 1 0 auto
           height 0
-        }
-      }
-       &.slide-4{
+      &.slide-4
         background-color #f0f
-      }
-       &.slide-5{
+        .scroll-wrapper
+          position fixed
+          top 46px
+          width 100%
+          bottom 0
+      &.slide-5
         background-color #00f
-      }
-    }
-  }
-}
 </style>
